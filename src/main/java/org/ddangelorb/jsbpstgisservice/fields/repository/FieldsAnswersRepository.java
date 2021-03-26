57bdcb3aaaefb1ebc4ca0b263389b1f36f659b9e
Daniel D'Angelo Resende Barros <ddangelorb@outlook.com>
2021-03-26 16:27:29 -0300

    Adding the first source code

---
+++ src/main/java/org/ddangelorb/jsbpstgisservice/fields/repository/FieldsAnswersRepository.java
@@ -0,0 +1,8 @@
+package org.ddangelorb.jsbpstgisservice.fields.repository;
+
+import org.ddangelorb.jsbpstgisservice.fields.model.FieldsAnswers;
+import org.springframework.data.repository.CrudRepository;
+
+public interface FieldsAnswersRepository extends CrudRepository<FieldsAnswers, Integer> {
+
+}
