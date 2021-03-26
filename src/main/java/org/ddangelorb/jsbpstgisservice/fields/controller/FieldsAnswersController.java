81479da189bb80afbf42c590860f1329fc040091
Daniel D'Angelo Resende Barros <ddangelorb@outlook.com>
2021-03-26 16:44:02 -0300

    Adding the first source code

---
+++ src/main/java/org/ddangelorb/jsbpstgisservice/fields/controller/FieldsAnswersController.java
@@ -0,0 +1,38 @@
+package org.ddangelorb.jsbpstgisservice.fields.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.ddangelorb.jsbpstgisservice.fields.dto.FieldsAnswersDTO;
+import org.ddangelorb.jsbpstgisservice.fields.model.FieldsAnswers;
+import org.ddangelorb.jsbpstgisservice.fields.service.FieldsAnswersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path="/fieldsanswers")
+public class FieldsAnswersController {
+	@Autowired 
+	 private FieldsAnswersService fieldsAnswersService;
+	
+	@PostMapping
+	public ResponseEntity<Void> add(FieldsAnswersDTO dto) {
+		fieldsAnswersService.add(dto.transformToObject());
+		return new ResponseEntity<Void>(HttpStatus.OK); 
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> addAll(String formcode, List<FieldsAnswersDTO> listDTO) {
+		List<FieldsAnswers> listFieldsAnswers = new ArrayList<FieldsAnswers>();
+		for (FieldsAnswersDTO dto : listDTO) {
+			listFieldsAnswers.add(dto.transformToObject());
+		}
+		
+		fieldsAnswersService.addAll(formcode, listFieldsAnswers);
+		return new ResponseEntity<Void>(HttpStatus.OK);
+	}
+}
